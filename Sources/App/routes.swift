4b572f25d413071cdc6a145355dc81a531f0c8a1
Akash Rastogi <akash_rastogi@live.com>
2021-01-10 16:43:56 +0800

    Initialize validation-docs

---
+++ Sources/App/routes.swift
@@ -0,0 +1,23 @@
+import Routing
+import Vapor
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+
+    router.post(User.self, at: "users") { req, user -> User in
+        try user.validate()
+        return user
+    }
+}
