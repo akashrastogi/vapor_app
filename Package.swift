f9523fa2c534a263dfc98da2af2cf117e5ff26ab
Akash Rastogi <akash_rastogi@live.com>
2021-01-10 16:43:51 +0800

    Initialize psql

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "VaporApp",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", .branch("nio")),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", .branch("nio")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
---
+++ Sources/App/configure.swift
@@ -0,0 +1,38 @@
+import FluentPostgreSQL
+import Vapor
+
+/// Called before your application initializes.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
+    // Register providers first
+    try services.register(FluentPostgreSQLProvider())
+
+    // Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    // Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+
+    // Configure a SQLite database
+    var databases = DatabaseConfig()
+    databases.add(database: PostgreSQLDatabase(config: .default()), as: .psql)
+    services.register(databases)
+
+    // Configure migrations
+    var migrations = MigrationConfig()
+    migrations.add(model: Todo.self, database: .psql)
+    services.register(migrations)
+
+    // Configure the rest of your application here
+}
